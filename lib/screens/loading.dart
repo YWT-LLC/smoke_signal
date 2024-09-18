d87a9ac42b5c5ce23d89a70a230d079b5c986418
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-18 13:29:27 -0400

    modernized

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -4,40 +4,41 @@
  */
 
 import '../utils/export.dart';
-
-import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import '../widgets/export.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 class LoadingScreen extends StatefulWidget {
-  const LoadingScreen({Key? key}) : super(key: key);
+  const LoadingScreen({super.key});
 
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  late double buttonSpacer = EzConfig.prefs[buttonSpacingKey];
+  // Gather theme data //
+
+  late final Lang l10n = Lang.of(context)!;
+
+  // Set the page title //
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setPageTitle('Loading');
+  }
+
+  // Return the build //
 
   @override
   Widget build(BuildContext context) {
-    return EzScaffold(
-      background: BoxDecoration(color: Color(EzConfig.prefs[backColorKey])),
-      appBar: EzAppBar(
-        title: Text(
-          appTitle,
-          style: buildTextStyle(styleKey: titleStyleKey),
-        ),
-      ),
-      body: ezView(
-        context: context,
-        background: BoxDecoration(
-          image: DecorationImage(image: EzImage.getProvider(backImageKey)),
-        ),
-        body: loadingMessage(
-          context: context,
-          image: EzImage(prefsKey: signalImageKey),
+    return const SmokeSignalScaffold(
+      drawerHeader: standardDrawerHeader,
+      body: EzScreen(
+        child: EzImage(
+          image: signalGif,
+          semanticLabel: 'Loading',
         ),
       ),
     );
