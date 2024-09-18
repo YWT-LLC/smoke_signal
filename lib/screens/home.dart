6c05966fcf535a885bb2e223ec7a5e9608dcc805
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-18 13:52:36 -0400

    modernized

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,22 +4,18 @@
  */
 
 import 'export.dart';
-
-import 'package:empathetech_ss_api/empathetech_ss_api.dart';
-import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+import '../../utils/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:empathetech_ss_api/empathetech_ss_api.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 /// Enumerator for communicating with [HomeScreen] build should be returned
-enum HomeBuildType {
-  loading,
-  auth,
-  app,
-}
+enum HomeBuildType { loading, auth, app }
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -29,37 +25,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Stream<User?> _authStream;
+  // Gather theme data //
 
-  /// Request permission for Firebase Cloud Messager
-  /// via [AppUser.messager]
-  _requestFCMPermission() async {
-    await AppUser.messager.requestPermission(
-      alert: true,
-      announcement: true,
-      badge: true,
-      sound: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-    );
-  }
+  late final Lang l10n = Lang.of(context)!;
 
-  @override
-  void initState() {
-    super.initState();
-    _authStream = AppUser.auth.authStateChanges();
-    _requestFCMPermission();
-  }
+  // Define build data //
 
-  late Color themeColor = Color(EzConfig.prefs[themeColorKey]);
-  late Color buttonColor = Color(EzConfig.prefs[buttonColorKey]);
-  late Color themeTextColor = Color(EzConfig.prefs[themeTextColorKey]);
-
-  late double buttonSpacer = EzConfig.prefs[buttonSpacingKey];
+  late Stream<User?> authStream;
 
   /// Updates current build based on the current auth [type]
-  Widget _getBuild(HomeBuildType type) {
+  Widget getBuild(HomeBuildType type) {
     switch (type) {
       case HomeBuildType.app:
         return const SignalBoard();
@@ -73,48 +48,64 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  // Init //
+
+  /// Request permission for Firebase Cloud Messager
+  /// via [AppUser.messager]
+  Future<NotificationSettings> requestFCMPermission() =>
+      AppUser.messager.requestPermission(
+        alert: true,
+        announcement: true,
+        badge: true,
+        sound: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    authStream = AppUser.auth.authStateChanges();
+    requestFCMPermission();
+  }
+
+  // Set the page title //
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setPageTitle('Home');
+  }
+
+  // Return the build //
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: _authStream,
-      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+    return StreamBuilder<User?>(
+      stream: authStream,
+      builder: (BuildContext sBContext, AsyncSnapshot<User?> snapshot) {
         switch (snapshot.connectionState) {
-          // Waiting on response
           case ConnectionState.waiting:
-            return _getBuild(HomeBuildType.loading);
+            return getBuild(HomeBuildType.loading);
 
-          // Received response
           case ConnectionState.done:
           default:
             // Check for user data, show auth build if invalid
-            if (snapshot.hasError || !snapshot.hasData)
-              return _getBuild(HomeBuildType.auth);
+            if (snapshot.hasError || !snapshot.hasData) {
+              return getBuild(HomeBuildType.auth);
+            }
 
-            User? currUser = snapshot.data;
-            if (currUser == null) return _getBuild(HomeBuildType.auth);
+            final User? currUser = snapshot.data;
+            if (currUser == null) return getBuild(HomeBuildType.auth);
 
-            // User is found!
-
-            // Merge them with the user DB and initialize local class
+            // Merge current user with DB and initialize local class
             setToken(currUser);
             AppUser.account = currUser;
             AppUser.db = FirebaseFirestore.instance;
 
-            // Setup a message listener
-
-            // Listeners aren't tied to screens!
-            // Notifs will be received on any page hereafter
-            FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-              await EzNotifications().show(
-                id: 0,
-                title: message.notification?.title,
-                body: message.notification?.body,
-              );
-            });
-
             // Load Smoke Signal!
-
-            return _getBuild(HomeBuildType.app);
+            return getBuild(HomeBuildType.app);
         }
       },
     );
