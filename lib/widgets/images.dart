a17bf260892e51e0a5e6c67d6e317a6ec320dae1
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-18 12:35:41 -0400

    added signalGif

---
+++ lib/widgets/images.dart
@@ -0,0 +1,11 @@
+/* smoke_signal
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import '../utils/export.dart';
+
+import 'package:flutter/material.dart';
+
+/// [AssetImage] of [smokeSignalPath]
+const ImageProvider signalGif = AssetImage(smokeSignalPath);
