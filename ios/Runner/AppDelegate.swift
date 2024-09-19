03dad3e20a6bf0c0930c4d6360a3226ed588043c
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-18 23:28:03 -0400

    managed updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,7 +4,7 @@ import Firebase
 import FirebaseMessaging
 import flutter_local_notifications
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
