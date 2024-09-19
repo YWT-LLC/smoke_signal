01dc2420407a3b88fcf8670c8d6bb8994bfca313
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-19 09:34:40 -0400

    added testing framework

---
+++ integration_test/fr_test.dart
@@ -0,0 +1,39 @@
+/* smoke_signal
+ * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'screens/home.dart' as home;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
+
+void main() async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final Map<String, Object> testConfig = <String, Object>{
+    ...empathetechConfig,
+    isDarkThemeKey: true,
+    localeKey: <String>['fr'],
+  };
+
+  SharedPreferences.setMockInitialValues(testConfig);
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  EzConfig.init(
+    assetPaths: <String>{},
+    preferences: prefs,
+    defaults: testConfig,
+  );
+
+  group(
+    'fr-locale',
+    () {
+      home.testSuite(locale: french);
+    },
+  );
+}
