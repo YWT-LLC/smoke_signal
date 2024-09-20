5a65249368d6324519ee072247aa0cc8a04fd193
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-20 14:13:10 -0400

    smushed

--- lib/screens/account/auth.dart
+++ lib/screens/account/auth.dart
@@ -9,8 +9,8 @@ import '../../utils/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:empathetech_ss_api/empathetech_ss_api.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -22,7 +22,18 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   // Gather theme data //
 
+  static const EzSpacer spacer = EzSpacer();
+  static const EzSeparator separator = EzSeparator();
+
   late final Lang l10n = Lang.of(context)!;
+
+  // Define build data //
+
+  final GlobalKey<FormState> emailFormKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> passwordFormKey = GlobalKey<FormState>();
+
+  late final TextEditingController emailController = TextEditingController();
+  late final TextEditingController passwdController = TextEditingController();
 
   // Set the page title //
 
@@ -43,23 +54,111 @@ class _AuthScreenState extends State<AuthScreen> {
         child: EzScrollView(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Login
-            ElevatedButton.icon(
-              onPressed: () => context.goNamed(loginPath),
-              icon: Icon(PlatformIcons(context).mail),
-              label: const Text('Login'),
-            ),
-            const EzSpacer(),
+            AutofillGroup(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // Email field
+                  ConstrainedBox(
+                    constraints: textFieldConstraints(context),
+                    child: TextFormField(
+                      key: emailFormKey,
+                      controller: emailController,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter email'),
+                      autofillHints: const <String>[AutofillHints.email],
+                      validator: emailValidator,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                    ),
+                  ),
+                  spacer,
 
-            // Sign up
-            ElevatedButton.icon(
-              onPressed: () => context.goNamed(signUpPath),
-              icon: Icon(PlatformIcons(context).mail),
-              label: const Text('Sign up'),
+                  // Password field
+                  ConstrainedBox(
+                    constraints: textFieldConstraints(context),
+                    child: TextFormField(
+                      key: passwordFormKey,
+                      controller: passwdController,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter password'),
+                      obscureText: true,
+                      autofillHints: const <String>[AutofillHints.password],
+                    ),
+                  ),
+                ],
+              ),
             ),
+            separator,
+
+            // Buttons
+            EzRowCol.sym(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                // Sign up
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    closeKeyboard(context);
+
+                    // Don't do anything if the input is invalid
+                    if (!emailFormKey.currentState!.validate()) {
+                      logAlert(context, message: 'Invalid email!');
+                      return;
+                    }
+
+                    // Attempt login
+                    await attemptAccountCreation(
+                      context,
+                      emailController.text.trim(),
+                      passwdController.text.trim(),
+                    );
+                  },
+                  icon: const Icon(Icons.edit_note_rounded),
+                  label: const Text('Sign up'),
+                ),
+                const EzSwapSpacer(),
+
+                // Login
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    closeKeyboard(context);
+
+                    // Don't attempt login if we know the input is invalid
+                    if (!emailFormKey.currentState!.validate()) {
+                      logAlert(context, message: 'Invalid email!');
+                      return;
+                    }
+
+                    await attemptLogin(
+                      context,
+                      emailController.text.trim(),
+                      passwdController.text.trim(),
+                    );
+                  },
+                  icon: const Icon(Icons.login),
+                  label: const Text('Login'),
+                ),
+              ],
+            ),
+            separator,
+
+            // Forgot password
+            EzLink(
+              'Forgot your password?',
+              style: Theme.of(context).textTheme.bodyLarge,
+              onTap: () => context.goNamed(resetPasswordPath),
+              semanticsLabel: 'Forgot your password?',
+            ),
+            spacer,
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwdController.dispose();
+    super.dispose();
   }
 }
