824f28ecda66471bb70006146249ae01e53cd310
empathictech <105748722+empathictech@users.noreply.github.com>
2024-10-07 16:28:02 -0400

    added fit defaults

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -25,9 +25,9 @@ const double signalCountHeight = 100.0;
 final Map<String, Object> ssDefaults = <String, Object>{
   ...empathetechConfig,
   darkBackgroundImageKey: darkForestPath,
-  '$darkBackgroundImageKey$boxFitSuffix': fill,
+  '$darkBackgroundImageKey$boxFitSuffix': fitHeight,
   lightBackgroundImageKey: lightForestPath,
-  '$lightBackgroundImageKey$boxFitSuffix': fill,
+  '$lightBackgroundImageKey$boxFitSuffix': fitHeight,
   darkTextBackgroundOKey: 0.35,
   lightTextBackgroundOKey: 0.70,
   signalImageKey: smokeSignalPath,
