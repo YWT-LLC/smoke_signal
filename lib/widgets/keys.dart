2c3a0efe6f757207a50c30e67eeb909527e56fd0
empathictech <105748722+empathictech@users.noreply.github.com>
2024-12-26 12:28:13 -0800

    removed unused

--- lib/widgets/keys.dart
+++
@@ -1,9 +0,0 @@
-/* smoke_signal
- * Copyright (c) 2022-2024 Empathetech LLC. All rights reserved.
- * See LICENSE for distribution and usage details.
- */
-
-import 'package:flutter/material.dart';
-
-final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
-    GlobalKey<ScaffoldMessengerState>();
