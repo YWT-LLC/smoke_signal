58b000ad2bb4d727ab5f9c548a73c4b9bbc0c57c
empathictech <105748722+empathictech@users.noreply.github.com>
2026-01-29 18:34:39 -0500

    removed unused

--- test_driver/integration_test_driver.dart
+++
@@ -1,8 +0,0 @@
-/* smoke_signal
- * Copyright (c) 2026 Empathetech LLC. All rights reserved.
- * See LICENSE for distribution and usage details.
- */
-
-import 'package:integration_test/integration_test_driver.dart';
-
-Future<void> main() => integrationDriver();
